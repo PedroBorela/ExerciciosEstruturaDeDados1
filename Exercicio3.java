8b975e680c801fec50309cb00361409f265337a0
PedroBorela <142453565+PedroBorela@users.noreply.github.com>
2024-10-24 08:21:42 -0300

    Update Exercicio3.java

--- Exercicio3.java
+++ Exercicio3.java
@@ -6,13 +6,17 @@ public class Exercicio3{
 
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
+        try{
         System.out.println("\nDigite um número: ");
         int num = sc.nextInt();
-        System.out.printf(primo(num,2)? "O número %d é primo" : "O número %d não é primo",num);
+        System.out.printf(primo(num)? "O número %d é primo\n" : "O número %d não é primo\n",num);
 
 
 
         sc.close();
+        }catch(Exception e){
+            System.out.println("Não é uma entrada válida");
+        }
     }
 
     public static boolean primo(int num,int div){
@@ -26,6 +30,10 @@ public class Exercicio3{
                 }
         }
         return primo(num,div+1);
+    }
+
+    public static boolean primo(int num){
+        return primo(num,2);
     }
 
 }
