d19ba679c81bc96c3c37afae45e0527abd5c88bb
PedroBorela <142453565+PedroBorela@users.noreply.github.com>
2024-10-24 08:48:01 -0300

    Update Exercicio5.java

--- Exercicio5.java
+++ Exercicio5.java
@@ -1,20 +1,27 @@
 import java.util.*;
 
-public class Exercicio5 {
+public class Exercicios {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        try{
         System.out.println("\nDigite um número: ");
         int num = sc.nextInt();
         System.out.println("\nDigite um número: ");
         int num2 = sc.nextInt();
 
-        System.out.printf("O mdc dos números %d e %d é %d", num, num2, mdc(num, num2));
+        System.out.printf("O mdc dos números %d e %d é %d\n", num, num2, mdc(num, num2));
 
         sc.close();
+        }catch(Exception e){
+            System.out.println("Não é uma entrada válida");
+        }
     }
 
     public static int mdc(int num, int num2) {
+        num = Math.abs(num);
+        num2 = Math.abs(num2);
+
         if (num >= num2 && num%num2 == 0)
             return num2;
         else {
