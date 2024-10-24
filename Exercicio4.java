f143aa474ac955a1b112f7d14f19b905c74d3ca6
PedroBorela <142453565+PedroBorela@users.noreply.github.com>
2024-10-24 08:30:40 -0300

    Update Exercicio4.java

--- Exercicio4.java
+++ Exercicio4.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
-public class Exercicio4 {
+public class Exercicio4{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        try{
 
         System.out.println("Insira o primeiro número: ");
         int n1= sc.nextInt();
@@ -11,8 +12,11 @@ public class Exercicio4 {
         int n2= sc.nextInt();
 
         int resultado= soma(n1, n2);
-        System.out.println(resultado);
+        System.out.printf("O resultado da multiplicação através de soma entre %d e %d é %d\n",n1,n2,resultado);
         sc.close();
+        }catch(Exception e){
+            System.out.println("Não é uma entrada válida");
+        }
     }
 
     public static int soma(int num,int num2){
@@ -22,6 +26,9 @@ public class Exercicio4 {
         if(num2>0){
 
             return num+soma(num,num2-1);
+        }else if(num2<0){
+
+            return -(num+soma(num,(-num2)-1));
         }
         
         return num;
