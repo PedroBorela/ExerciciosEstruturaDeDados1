ddf4d77c04d17977323ca4f322856abc338dd58c
PedroBorela <142453565+PedroBorela@users.noreply.github.com>
2024-12-07 11:29:49 -0300

    Update Pilha.java

--- Pilha.java
+++ Pilha.java
@@ -1,6 +1,6 @@
 public class Pilha{
     static final int MAX_TAM =100;
-    int[] pilha = new int[MAX_TAM];
+    String[] pilha = new String[MAX_TAM];
     int topo=-1;
     public boolean isVazia(){
         return topo==-1;
@@ -8,19 +8,19 @@ public class Pilha{
     public int getTamanho(){
         return topo+1;
     }
-    public void push(int p ) throws Exception{
+    public void push(String p ) throws Exception{
         if(topo==MAX_TAM-1)
         throw new Exception("Não há espaços disponíves");
         pilha[++topo]=p;
 
     }
-    public int top() throws Exception{
+    public String top() throws Exception{
         if(isVazia())
         throw new Exception("lista vazia");
         return pilha[topo];
     }
 
-    public int pop()throws Exception{
+    public String pop()throws Exception{
         if(isVazia())
         throw new Exception("Lista vazia");
         return pilha[topo--];
